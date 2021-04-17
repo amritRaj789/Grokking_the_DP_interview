e7ab4237705dd0c3f4b7fc8f503ac375bb6b3d55
amritRaj789 <amritrazzz@gmail.com>
2021-04-17 17:14:05 +0530

    --longest palindromic substring

--- Palindromic Subsequence.js
+++ Palindromic Subsequence.js
@@ -46,7 +46,6 @@ let findLPSLength = function (st){
 	let dp = Array(st.length).fill(null).map(() => Array(st.length).fill(0));
 	for(let i = 0; i < st.length; i++)
 		dp[i][i] = 1;
-
 	for(let start = st.length-1; start >= 0; start--){
 		for(let end = start+1; end < st.length; end++){
 			if(st[start] === st[end]){
@@ -56,6 +55,67 @@ let findLPSLength = function (st){
 				dp[start][end] = Math.max(dp[start+1][end], dp[start][end-1]);
 		}
 	}
-
 	return dp[0][st.length-1];
 }
+// O(n^2)
+
+
+
+
+// Longest Palindromic Substring
+
+/*Given a string, find the length of its Longest Palindromic Substring (LPS). 
+In a palindromic string, elements read the same backward and forward.*/
+
+// simple recursive
+let findLPSLength = function(st){
+	function recursive(left, right){
+		if(left > right){
+			return 0
+		}
+		if(left === right){
+			return 1
+		}
+		if(st[left] === st[right]){
+			let remaining = right - left - 1;
+			if(remaining === recursive(left+1, right-1))
+				return remaining+2;
+		}
+		return Math.max(recursive(left+1, right), recursive(left, right-1));
+	}
+	return recursive(0, st.length-1);
+}
+
+// top down
+
+
+
+
+
+// bottom up DP
+let findLPSLength = function (st){
+	let max = 1;
+	let dp = Array(st.length).fill(null).map(() => Array(st.length).fill(0));
+	for(let i = 0; i < st.length; i++)
+		dp[i][i] = 1;
+	for(let start = st.length-1; start >= 0; start--){
+		for(let end = start+1; end < st.length; end++){
+			if(st[start] === st[end]){
+				dp[start][end] = dp[start+1][end-1] + 2;
+				max = Math.max(max, dp[start][end]);
+			}
+			else
+				dp[start][end] = -Infinity;
+		}
+	}
+	return max;
+}
+
+//this uses O(N^2)
+
+// my leetcode solution is a bit better may be
+
+
+// THERE IS AN ASTONISHINGLY MORE EFFICIENT LINEAR TIME ALGORITHM : MANACHER'S ALGO
+// read more on it later
+// imma go grab a diet coke first
