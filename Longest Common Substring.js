be9959ec68e9eb90e78ea3fb41dae883b4add55c
amritRaj789 <amritrazzz@gmail.com>
2021-04-22 17:45:27 +0530

    --Edit distance

--- Longest Common Substring.js
+++ Longest Common Substring.js
@@ -651,3 +651,29 @@ const findLASLength = function (nums){
 }
 
 // fuck yeah yeah yeah!!!! nailed it 
+
+
+
+// Edit distance
+/*Given strings s1 and s2, we need to transform s1 into s2 by deleting, inserting, or replacing characters. 
+Write a function to calculate the count of the minimum number of edit operations.
+*/
+
+// skipped recursive
+
+const findMinOperations = function(s1, s2) {
+	let dp = Array(s1.length+1).fill(null).map(() => Array(s2.length+1).fill(0));
+	for(let i = 0; i <= s1.length; i++)
+		dp[i][0] = i;
+	for(let j = 0; j <= s2.length; j++)
+		dp[0][j] = j;
+	for(let i = 1; i <= s1.length; i++){
+		for(let j = 1; j <= s2.length; j++){
+			if(s1[i-1] === s2[j-1])
+				dp[i][j] = dp[i-1][j-1];
+			else
+				dp[i][j] = 1 + Math.min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+		}
+	}
+	return dp[s1.length][s2.length];
+};
