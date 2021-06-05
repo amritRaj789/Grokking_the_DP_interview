8a5064c2f0915cc30b16991c8b2a7a216adc6496
amritRaj789 <amritrazzz@gmail.com>
2021-06-05 09:32:59 +0530

    --fixed typos

--- Palindromic Subsequence.js
+++ Palindromic Subsequence.js
@@ -6,63 +6,57 @@ A subsequence is a sequence that can be derived from another sequence by deletin
 without changing the order of the remaining elements.*/
 
 // my recursive solution; educative also does something similar
-let findLPSLength = function (st){
-	function recursive(left, right){
-		if(left > right)
-			return 0
-		if(st[left] === st[right]){
-			return recursive(left+1, right-1) + (left < right ? 2 : 1);
-		}
-		else{
-			return Math.max(recursive(left+1, right), recursive(left, right-1));
-		}
-	}
-	return recursive(0, st.length-1);
-}
+let findLPSLength = function (st) {
+  function recursive(left, right) {
+    if (left > right) return 0;
+    if (st[left] === st[right]) {
+      return recursive(left + 1, right - 1) + (left < right ? 2 : 1);
+    } else {
+      return Math.max(recursive(left + 1, right), recursive(left, right - 1));
+    }
+  }
+  return recursive(0, st.length - 1);
+};
 
-console.log(findLPSLength("bdsgjbdb"))
+console.log(findLPSLength("bdsgjbdb"));
 
 //top down with memoization
-let findLPSLength = function (st){
-	let memo = [];
-	function recursive(left, right){
-		if(memo[left] !== undefined && memo[left][right] !== undefined)
-			return memo[left][right];
-		if(left > right)
-			return 0
-		if(left === right)
-			return 1
-		memo[left] = memo[left] || [];
-		if(st[left] === st[right])
-			memo[left][right] = recursive(left+1, right-1) + 2;
-		else
-			memo[left][right] = Math.max(recursive(left+1, right), recursive(left, right-1));
-		return memo[left][right];
-	}
-	return recursive(0, st.length-1);
-}
-
+let findLPSLength = function (st) {
+  let memo = [];
+  function recursive(left, right) {
+    if (memo[left] !== undefined && memo[left][right] !== undefined)
+      return memo[left][right];
+    if (left > right) return 0;
+    if (left === right) return 1;
+    memo[left] = memo[left] || [];
+    if (st[left] === st[right])
+      memo[left][right] = recursive(left + 1, right - 1) + 2;
+    else
+      memo[left][right] = Math.max(
+        recursive(left + 1, right),
+        recursive(left, right - 1)
+      );
+    return memo[left][right];
+  }
+  return recursive(0, st.length - 1);
+};
 
 // bottom up dynamic programming..
-let findLPSLength = function (st){
-	let dp = Array(st.length).fill(null).map(() => Array(st.length).fill(0));
-	for(let i = 0; i < st.length; i++)
-		dp[i][i] = 1;
-	for(let start = st.length-1; start >= 0; start--){
-		for(let end = start+1; end < st.length; end++){
-			if(st[start] === st[end]){
-				dp[start][end] = dp[start+1][end-1] + 2;
-			}
-			else
-				dp[start][end] = Math.max(dp[start+1][end], dp[start][end-1]);
-		}
-	}
-	return dp[0][st.length-1];
-}
+let findLPSLength = function (st) {
+  let dp = Array(st.length)
+    .fill(null)
+    .map(() => Array(st.length).fill(0));
+  for (let i = 0; i < st.length; i++) dp[i][i] = 1;
+  for (let start = st.length - 1; start >= 0; start--) {
+    for (let end = start + 1; end < st.length; end++) {
+      if (st[start] === st[end]) {
+        dp[start][end] = dp[start + 1][end - 1] + 2;
+      } else dp[start][end] = Math.max(dp[start + 1][end], dp[start][end - 1]);
+    }
+  }
+  return dp[0][st.length - 1];
+};
 // O(n^2)
-
-
-
 
 // Longest Palindromic Substring
 
@@ -70,63 +64,50 @@ let findLPSLength = function (st){
 In a palindromic string, elements read the same backward and forward.*/
 
 // simple recursive
-let findLPSLength = function(st){
-	function recursive(left, right){
-		if(left > right){
-			return 0
-		}
-		if(left === right){
-			return 1
-		}
-		if(st[left] === st[right]){
-			let remaining = right - left - 1;
-			if(remaining === recursive(left+1, right-1))
-				return remaining+2;
-		}
-		return Math.max(recursive(left+1, right), recursive(left, right-1));
-	}
-	return recursive(0, st.length-1);
-}
+let findLPSLength = function (st) {
+  function recursive(left, right) {
+    if (left > right) {
+      return 0;
+    }
+    if (left === right) {
+      return 1;
+    }
+    if (st[left] === st[right]) {
+      let remaining = right - left - 1;
+      if (remaining === recursive(left + 1, right - 1)) return remaining + 2;
+    }
+    return Math.max(recursive(left + 1, right), recursive(left, right - 1));
+  }
+  return recursive(0, st.length - 1);
+};
 
 // top down
 
-
-
-
-
 // bottom up DP
-let findLPSLength = function (st){
-	let max = 1;
-	let dp = Array(st.length).fill(null).map(() => Array(st.length).fill(0));
-	for(let i = 0; i < st.length; i++)
-		dp[i][i] = 1;
-	for(let start = st.length-1; start >= 0; start--){
-		for(let end = start+1; end < st.length; end++){
-			if(st[start] === st[end]){
-				dp[start][end] = dp[start+1][end-1] + 2;
-				max = Math.max(max, dp[start][end]);
-			}
-			else
-				dp[start][end] = -Infinity;
-		}
-	}
-	return max;
-}
+let findLPSLength = function (st) {
+  let max = 1;
+  let dp = Array(st.length)
+    .fill(null)
+    .map(() => Array(st.length).fill(0));
+  for (let i = 0; i < st.length; i++) dp[i][i] = 1;
+  for (let start = st.length - 1; start >= 0; start--) {
+    for (let end = start + 1; end < st.length; end++) {
+      if (st[start] === st[end]) {
+        dp[start][end] = dp[start + 1][end - 1] + 2;
+        max = Math.max(max, dp[start][end]);
+      } else dp[start][end] = -Infinity;
+    }
+  }
+  return max;
+};
 
 //this uses O(N^2)
 
 // my leetcode solution is a bit better may be
 
-
 // THERE IS AN ASTONISHINGLY MORE EFFICIENT LINEAR TIME ALGORITHM : MANACHER'S ALGO
 // read more on it later
 // imma go grab a diet coke first
-
-
-
-
-
-
 
 // COUNT OF PALINDROMIC SUBSTRINGS
 
@@ -135,37 +116,31 @@ Please note we need to find the total number of substrings and not subsequences.
 
 // Idk why educative has no recursive solution for this
 // this is their bottom up DP solution
-let findCPS = function (st){
-	let count = 0;
-	let dp = Array(st.length).fill(null).map(() => Array(st.length).fill(false));
-	for(let i = 0; i < st.length; i++){
-		dp[i][i] = true;
-		count++;
-	}
-	for(let start = st.length-1; start >= 0; start--){
-		for(let end = start+1; end < st.length; end++){
-			if(st[end] === st[start]){
-				if(end - start === 1 || dp[start+1][end-1]){
-					dp[start][end] = true;
-					count++;
-				}
-			}
-		}
-	}
-	return count;
-}
-console.log('Length of LPS: ---> ' + findCPS('pqr'));
+let findCPS = function (st) {
+  let count = 0;
+  let dp = Array(st.length)
+    .fill(null)
+    .map(() => Array(st.length).fill(false));
+  for (let i = 0; i < st.length; i++) {
+    dp[i][i] = true;
+    count++;
+  }
+  for (let start = st.length - 1; start >= 0; start--) {
+    for (let end = start + 1; end < st.length; end++) {
+      if (st[end] === st[start]) {
+        if (end - start === 1 || dp[start + 1][end - 1]) {
+          dp[start][end] = true;
+          count++;
+        }
+      }
+    }
+  }
+  return count;
+};
+console.log("Length of LPS: ---> " + findCPS("pqr"));
 // O(n^2)
 
-
-
-
-
-
-
-
 // Minimum Deletions in a string to make it palindrome
-
 
 //Given a string, find the minimum number of characters that we can remove to make it a palindrome.
 
@@ -173,42 +148,33 @@ console.log('Length of LPS: ---> ' + findCPS('pqr'));
 
 // bottom up DP
 
-let findMinimumDeletions = function (st){
-	let dp = Array(st.length).fill(null).map(() => Array(st.length).fill(0));
+let findMinimumDeletions = function (st) {
+  let dp = Array(st.length)
+    .fill(null)
+    .map(() => Array(st.length).fill(0));
 
-	for(let i = 0; i < st.length; i++){
-		dp[i][i] = 1;
-	}
+  for (let i = 0; i < st.length; i++) {
+    dp[i][i] = 1;
+  }
 
-	for(let start = st.length-1; start >= 0; start--){
-		for(let end = start+1; end < st.length; end++){
-			if(st[start] === st[end])
-				dp[start][end] = 2 + dp[start+1][end-1];
-			else
-				dp[start][end] = Math.max(dp[start+1][end], dp[start][end-1]);
-		}
-	}
+  for (let start = st.length - 1; start >= 0; start--) {
+    for (let end = start + 1; end < st.length; end++) {
+      if (st[start] === st[end]) dp[start][end] = 2 + dp[start + 1][end - 1];
+      else dp[start][end] = Math.max(dp[start + 1][end], dp[start][end - 1]);
+    }
+  }
 
-	return st.length - dp[0][st.length-1];
-}
+  return st.length - dp[0][st.length - 1];
+};
 // O(n^2)
 
-
-// This question will also work for similar questions such as: 
-
+// This question will also work for similar questions such as:
 
 // Minimum insertions in a string to make it a palindrome
-
 
 // Find if a string is K-Palindromic
 // Any string is K palindromic if it can be transformed into a palindrome
 //by removing at most K elements
-
-
-
-
-
-
 
 // Palindromic Partitioning
 
@@ -218,39 +184,37 @@ Write a function to return the minimum number of cuts needed.*/
 // this is by far the worst question in this course. The creators have done a lousy job in explaining
 // it is also full with mistakes or lapses in explanations
 
-#red #red #red #red #red #red #red #red #red #red #red
-let findMPPCuts = function(st){
-	function recursive(start,end){
-		if(start >= end || isPalindromic(start, end))
-			return 0
-		let minimumCuts = end - start;
-		for(let i = start; i <= end; i++){
-			if(isPalindromic(start, i)){
-				minimumCuts = Math.min(minimumCuts, 1 + recursive(i+1, end));
-			}
-		}
-		return minimumCuts;
-	}
+//#red #red #red #red #red #red #red #red #red #red #red
+let findMPPCuts = function (st) {
+  function recursive(start, end) {
+    if (start >= end || isPalindromic(start, end)) return 0;
+    let minimumCuts = end - start;
+    for (let i = start; i <= end; i++) {
+      if (isPalindromic(start, i)) {
+        minimumCuts = Math.min(minimumCuts, 1 + recursive(i + 1, end));
+      }
+    }
+    return minimumCuts;
+  }
 
-	function isPalindromic (left, right){
-		while(left <= right){
-			if(st[left] !== st[right])
-				return false
-			left++;
-			right--;
-		}
-		return true;
-	}
+  function isPalindromic(left, right) {
+    while (left <= right) {
+      if (st[left] !== st[right]) return false;
+      left++;
+      right--;
+    }
+    return true;
+  }
 
-	return recursive(0, st.length-1);
-}
-	
+  return recursive(0, st.length - 1);
+};
+
 // top down with memoization
-const findMPPCuts = function(st) {
-	let dp = [];
+const findMPPCuts = function (st) {
+  let dp = [];
   function findMPPCutsRecursive(st, startIndex, endIndex) {
-  	if(dp[startIndex] !== undefined && dp[startIndex][endIndex] !== undefined)
-  		return dp[startIndex][endIndex];
+    if (dp[startIndex] !== undefined && dp[startIndex][endIndex] !== undefined)
+      return dp[startIndex][endIndex];
     // we don't need to cut the string if it is a palindrome
     if (startIndex >= endIndex || isPalindrome(st, startIndex, endIndex)) {
       return 0;
@@ -261,7 +225,10 @@ const findMPPCuts = function(st) {
     for (let i = startIndex; i <= endIndex; i++) {
       if (isPalindrome(st, startIndex, i)) {
         // we can cut here as we have a palindrome from 'startIndex' to 'i'
-        minimumCuts = Math.min(minimumCuts, 1 + findMPPCutsRecursive(st, i + 1, endIndex));
+        minimumCuts = Math.min(
+          minimumCuts,
+          1 + findMPPCutsRecursive(st, i + 1, endIndex)
+        );
       }
     }
     dp[startIndex][endIndex] = minimumCuts;
@@ -269,14 +236,13 @@ const findMPPCuts = function(st) {
   }
   let dp2 = [];
   function isPalindrome(st, x, y) {
-  	if(dp2[x] !== undefined && dp2[x][y] !== undefined)
-  		return dp2[x][y];
-  	let copyX = x;
-  	let copyY = y;
-  	dp2[copyX] = dp2[copyX] || [];
+    if (dp2[x] !== undefined && dp2[x][y] !== undefined) return dp2[x][y];
+    let copyX = x;
+    let copyY = y;
+    dp2[copyX] = dp2[copyX] || [];
     while (x <= y) {
       if (st[x++] != st[y--]) {
-      	dp2[copyX][copyY] = false;
+        dp2[copyX][copyY] = false;
         return false;
       }
     }
@@ -287,6 +253,5 @@ const findMPPCuts = function(st) {
   return findMPPCutsRecursive(st, 0, st.length - 1);
 };
 
-
 // bottom up
-Incomplete I am frustrated by this problem!
+//Incomplete I am frustrated by this problem!
